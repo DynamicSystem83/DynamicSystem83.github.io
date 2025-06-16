228e8823dadebe160946591ade1897ca3f0fb2a9
DynamicSystem <yindynasty@gmail.com>
2025-06-15 20:14:02 -0500

    add button

--- apps/encryption.py
+++ apps/encryption.py
@@ -34,11 +34,16 @@ def _():
     input_text_area = mo.ui.text_area(placeholder="type some text to be encoded ...")
     input_text_area
 
+    encode_button = mo.ui.run_button("Encode")
+    encode_button
+
     return input_text_area
 
 
 @app.cell
-def _(input_text_area):
+def _(input_text_area, encode_button):
+    mo.stop(not encode_button.value)
+
     encoded_text = encode(input_text_area.value)
     mo.md(encoded_text)
     return
