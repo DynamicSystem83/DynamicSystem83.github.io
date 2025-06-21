964bbfb0b69ed6447481afdf68ad262c1944cdf4
DynamicSystem <yindynasty@gmail.com>
2025-06-21 16:23:13 -0500

    test

---
+++ apps/book_series.py
@@ -0,0 +1,54 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "marimo==0.13.15",
+#     "numpy==2.3.0",
+# ]
+# ///
+import marimo
+
+__generated_with = "0.10.9"
+app = marimo.App(width="medium")
+
+with app.setup:
+    import marimo as mo
+    import numpy as np
+    import xml.etree.ElementTree as et
+    import os
+
+
+@app.cell
+def _():
+    """
+    Input form cell.
+    """
+
+    entries = os.listdir('.')
+    print(entries)
+    entries = os.listdir('./__marimo__')
+    print(entries)
+    entries = os.listdir('./__marimo__/cache')
+    print(entries)
+    entries = os.listdir('../marimo')
+    print(entries)
+    entries = os.listdir('..')
+    print(entries)
+
+    file = mo.notebook_location() / "public" / "book_series.xml"
+    file = "public/book_series.xml"
+
+
+    with open(file, "r") as xml_file:
+        lines = xml_file.readlines()
+
+    print(lines)
+    output_text = "test"
+    output_text_area = mo.ui.code_editor(value=output_text)
+    output_text_area
+
+    return output_text_area
+
+
+if __name__ == "__main__":
+    app.run()
+
